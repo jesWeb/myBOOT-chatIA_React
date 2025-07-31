888dd4b76fd631fc817ab533df7b1096a42cf823
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-07-30 18:37:18 -0600

    hicximos responsivo el main

--- src/components/MainVentanaChat.tsx
+++ src/components/MainVentanaChat.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form"
 import { minLength, object, pipe, promise, string } from "valibot"
 import { valibotResolver } from "@hookform/resolvers/valibot";
 import { useChatStore } from "../store/useChartStore";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { consultorIA } from "../lib/consultarIA";
 
 
@@ -74,13 +74,11 @@ const MainVentanaChat = () => {
 
     }
 
+    const scrollRef = useRef<HTMLDivElement | null>(null)
 
-    // useEffect(() => {
-    //     consultorIA().then((respuesta) => {
-    //         console.log("Respuesta de la IA", respuesta);
-
-    //     })
-    // }, [])
+    useEffect(() => {
+        scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+    }, [mensaje])
 
     return (
         <div className="flex flex-col h-screen bg-zinc-900 text-white">
@@ -89,20 +87,31 @@ const MainVentanaChat = () => {
                 <h1 className="text-xl font-semibold">ARES IA</h1>
             </header>
 
-            <main className="flex-1 px-4 py-6 overflow-y-auto space-y-4">
-                {mensaje.map((mensa) => (
-                    <div key={mensa.id}
-                        className={`max-w-md px-4 py-2 rounded-xl shadow ${mensa.rol === "usuario" ? "bg-zinc-500 self-end" : "bg-zinc-700  self-start"}`}
-                    >
-                        {mensa.texto}
-                    </div>
-                ))}
+            <main className="flex-1 flex justify-center px-4 py-6 overflow-y-auto">
+                <div />
+                <div className="w-full max-w-3xl  flex flex-col space-y-4">
+                    {mensaje.map((mensa) => (
+                        <div key={mensa.id}
+                            className={`flex ${mensa.rol === "usuario" ? "justify-end" : "justify-center"}`}
+                        >
+                            <div className={
+                                `w-fit max-w-[90%] px-4 py-2 rounded-xl shadow
+                                 ${mensa.rol === "usuario" ? "bg-zinc-500 self-end" : "bg-zinc-700  self-start"}`}>
+                                {mensa.texto}
+                            </div>
 
-                {cargando && (
-                    <div className="italic text-gray-400">
-                        El boot esta escribiendo ...
-                    </div>
-                )}
+                        </div>
+                    ))}
+
+                    {cargando && (
+                        <div className="italic text-gray-400">
+                            El boot esta escribiendo ...
+                        </div>
+                    )}
+
+                    <div ref={scrollRef} />
+                </div>
+                <div />
             </main>
 
             <footer className="px-4 py-3 border-t border-zinc-700 space-y-4">
